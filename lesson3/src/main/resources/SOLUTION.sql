a23e011b48f38b9f67b96011d1f55b088d5ed5b9
Nikita Pyshchev <npyshchev@igatec.com>
2025-02-12 02:14:36 +0300

    fx

--- lesson3/src/main/resources/SOLUTION.sql
+++ lesson3/src/main/resources/SOLUTION.sql
@@ -1,5 +1,9 @@
 alter table student alter column birthday set NOT NULL;
-alter table mark add check (mark>=1 AND mark <=10), alter column student_id set NOT NULL, alter column subject_id set NOT NULL;
-alter table subject add check (grade>=1 AND grade <=5);
+alter table mark add check (mark>=1 AND mark <=10);
+alter table mark alter column student_id set NOT NULL;
+alter table mark alter column subject_id set NOT NULL;
+ALTER TABLE subject ADD CONSTRAINT grade_check CHECK (grade >= 1 AND grade <= 5);
 alter table PaymentType ADD UNIQUE (name);
-alter table Payment alter column type_id set NOT NULL, alter column amount set NOT NULL, alter column date set NOT NULL;
+alter table Payment alter column type_id set NOT NULL;
+alter table Payment alter column amount set NOT NULL;
+alter table Payment alter column date set NOT NULL;
