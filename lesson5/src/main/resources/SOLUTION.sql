cede4d2f3ec31ff8829708c39b879d2c2a2f1899
Nikita Pyshchev <npyshchev@igatec.com>
2025-02-14 02:23:53 +0300

    wip

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -1,7 +1,7 @@
 SELECT * FROM payment WHERE amount >= 500;
-SELECT * FROM student WHERE birthday < DATEADD(YEAR, -20, '20001010');
-SELECT * FROM student WHERE groupnumber = 10 AND birthday > DATEADD(YEAR, -20, '20001010');
+SELECT * FROM student WHERE birthday < DATEADD(YEAR, -20, '20250214');
+SELECT * FROM student WHERE groupnumber = 10 AND birthday > DATEADD(YEAR, -20, '20250214');
 SELECT * FROM student WHERE name = 'Mike' OR groupnumber IN (4, 5, 6);
-SELECT * FROM payment WHERE payment_date > DATEADD(MONTH, -8, '20001010');
+SELECT * FROM payment WHERE payment_date > DATEADD(MONTH, -8, '20250214');
 SELECT * FROM student WHERE name like 'A%';
 SELECT * FROM student WHERE (name = 'Roxi' AND groupnumber = 4) OR (name = 'Tallie' AND groupnumber = 9);
