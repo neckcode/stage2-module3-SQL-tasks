9b48f6c6d00462f3cadf9afc984d4faaf1b71a83
Nikita Pyshchev <npyshchev@igatec.com>
2025-02-16 19:35:48 +0300

    wip

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,7 +1,4 @@
 DELETE FROM student WHERE id IN (SELECT DISTINCT student_id FROM mark WHERE subject_id IN (SELECT id FROM subject WHERE grade >= 4));
-DELETE FROM mark WHERE student_id NOT IN (SELECT student_id FROM Student);
-DELETE FROM payment WHERE student_id NOT IN (SELECT student_id FROM Student);
 DELETE FROM student WHERE id IN(SELECT DISTINCT student_id FROM mark GROUP BY student_id HAVING MIN(mark)<4);
 DELETE FROM paymenttype WHERE name = 'DAILY';
-DELETE FROM payment WHERE type_id NOT IN (SELECT id FROM paymenttype);
 DELETE FROM mark WHERE mark < 7;
--- lesson12/src/main/resources/TABLES_CHANGES.sql
+++ lesson12/src/main/resources/TABLES_CHANGES.sql
@@ -1,1 +1,8 @@
-select * from student;
+ALTER TABLE mark DROP CONSTRAINT mark_student_id_fkey;
+ALTER TABLE mark DROP CONSTRAINT mark_subject_id_fkey;
+ALTER TABLE mark ADD CONSTRAINT mark_student_id_fkey FOREIGN KEY (student_id) references student(id) ON DELETE CASCADE;
+ALTER TABLE mark ADD CONSTRAINT mark_subject_id_fkey FOREIGN KEY (subject_id) references subject(id) ON DELETE CASCADE;
+ALTER TABLE payment DROP CONSTRAINT payment_type_id_fkey;
+ALTER TABLE payment DROP CONSTRAINT payment_student_id_fkey;
+ALTER TABLE payment ADD CONSTRAINT payment_type_id_fkey FOREIGN KEY (type_id) references paymenttype(id) ON DELETE CASCADE;
+ALTER TABLE payment ADD CONSTRAINT payment_student_id_fkey FOREIGN KEY (student_id) references student(id) ON DELETE CASCADE;
