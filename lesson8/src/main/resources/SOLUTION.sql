32acc15df91bd3d7614f2254d916e4933f923cdd
Nikita Pyshchev <npyshchev@igatec.com>
2025-02-16 19:42:26 +0300

    fx

--- lesson8/src/main/resources/SOLUTION.sql
+++ lesson8/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
 SELECT birthday FROM student ORDER BY birthday DESC LIMIT 1;
 SELECT MIN(payment_date) FROM payment;
-SELECT AVG(mark) FROM mark WHERE subject_id = (SELECT id FROM subject WHERE name = 'Math');
-SELECT MIN(amount) FROM payment WHERE type_id = (SELECT id FROM paymenttype WHERE name = 'WEEKLY')
+SELECT AVG(mark) FROM mark WHERE subject_id IN (SELECT id FROM subject WHERE name = 'Math');
+SELECT MIN(amount) FROM payment WHERE type_id IN (SELECT id FROM paymenttype WHERE name = 'WEEKLY')
